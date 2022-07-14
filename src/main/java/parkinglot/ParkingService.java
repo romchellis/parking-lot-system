6a76650deed516ff9cd3a9698f384e511a723128
romchellis <oborinroma@yandex.ru>
2022-07-14 15:39:19 +0300

    first commit

---
+++ src/main/java/parkinglot/ParkingService.java
@@ -0,0 +1,11 @@
+package parkinglot;
+
+/**
+ * We have to implement Parking service API which includes:
+ * 1) Register car
+ * 2) Out car
+ * 3) Get count of busy slots in currentMoment
+ * 4) Get hits in time interval ( from today 14.30 to 16.00 was 25 hits)
+ */
+public interface ParkingService {
+}
